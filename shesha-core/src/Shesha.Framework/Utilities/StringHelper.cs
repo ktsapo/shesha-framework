fd564a9aa9a3fe1d569c96e3a2a059ef0fa67b75
Robert-John Marshall <robert.marshall@boxfusion.io>
2026-04-29 13:48:33 +0200

    [change] string helper use valid Email logic already contained in the string helper

--- shesha-core/src/Shesha.Framework/Utilities/StringHelper.cs
+++ shesha-core/src/Shesha.Framework/Utilities/StringHelper.cs
@@ -864,13 +864,9 @@ namespace Shesha.Utilities
             return String.Join(" ", SplitUpperCase(source));
         }
 
-        private static readonly Regex ValidEmailExpression = new Regex(
-            @"^([a-zA-Z0-9_\-\.\+]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,}|[0-9]{1,3})(\]?)$",
-            RegexOptions.Compiled);
-
         public static bool IsValidEmail(this string inputEmail)
         {
-            return !string.IsNullOrWhiteSpace(inputEmail) && ValidEmailExpression.IsMatch(inputEmail);
+            return IsEmail(inputEmail);
         }
 
         [DebuggerStepThrough]

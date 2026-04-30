c42e29ea7a2400d7b188eecb14f7243bc24f7a93
czwe-01 <tnkambule980115@gmail.com>
2026-04-30 10:36:48 +0200

    refactor: simplify ActionCell component by removing unused imports and integrating available constants data
    - Integrated useAvailableConstantsData to enhance the evaluation context.

--- shesha-reactjs/src/components/dataTable/cell/actionCell.tsx
+++ shesha-reactjs/src/components/dataTable/cell/actionCell.tsx
@@ -1,20 +1,17 @@
-import { Tooltip, App } from 'antd';
-import moment from 'moment';
+import { Tooltip } from 'antd';
 import React from 'react';
 import { IconType, ShaIcon } from '@/components';
 import {
   isNavigationActionConfiguration,
   useConfigurableActionDispatcher,
   useDataTable,
-  useForm,
-  useGlobalState,
-  useHttpClient,
   useShaRouting,
 } from '@/providers';
 import { ITableActionColumn } from '@/providers/dataTable/interfaces';
 import { ICommonCellProps } from './interfaces';
 import Link from 'next/link';
 import { useAsyncMemo } from '@/hooks/useAsyncMemo';
+import { useAvailableConstantsData } from '@/providers/form/utils';
 
 
 export interface IActionCellProps<D extends object = {}, V = any> extends ICommonCellProps<ITableActionColumn, D, V> { }
@@ -22,28 +19,21 @@ export interface IActionCellProps<D extends object = {}, V = any> extends ICommo
 export const ActionCell = <D extends object = {}, V = any>(props: IActionCellProps<D, V>) => {
   const { columnConfig } = props;
   const { changeActionedRow } = useDataTable();
-  const httpClient = useHttpClient();
-  const { formData, formMode } = useForm();
-  const { globalState } = useGlobalState();
   const { executeAction, prepareArguments, useActionDynamicContext } = useConfigurableActionDispatcher();
   const { getUrlFromNavigationRequest } = useShaRouting();
-  const { message } = App.useApp();
 
   const { actionConfiguration, icon, description } = columnConfig ?? {};
   const dynamicContext = useActionDynamicContext(actionConfiguration);
+
+  const allData = useAvailableConstantsData();
 
   const getRowData = (data) => {
     return data?.cell?.row?.original;
   };
 
   const evaluationContext = {
+    ...allData,
     selectedRow: getRowData(props),
-    data: formData,
-    moment: moment,
-    formMode: formMode,
-    http: httpClient,
-    message: message,
-    globalState: globalState,
     ...dynamicContext
   };
 
